7066bdf75684aeb4a27cd90e925f7e7be1cd63db
Yuri Urentsov <yuri.urentsov@gmail.com>
2020-12-27 10:29:16 +0300

    PROJECT

---
+++ app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+from gender_project.model import *
+from gender_project.database_requests import *
+
+from flask import Flask
+from flask import jsonify
+
+
+# SQLAlchemy
+# Use following credentials:
+# hostname: relational.fit.cvut.cz
+# port: 3306
+# username: guest
+# password: relational
+
+# engine = create_engine("mysql://flask_user:password@localhost/ftp", echo=False)
+engine = create_engine("mysql://guest:relational@relational.fit.cvut.cz:3306/ftp", echo=False)
+connection = engine.connect()
+# create a configured "Session" class
+Session = sessionmaker(bind=engine)
+# create a Session
+session = Session()
+
+#load predictive model
+PATH_TO_DATA = './models/'
+with open(os.path.join(PATH_TO_DATA, 'A.pkl'), mode = 'rb') as file:
+    production_model = pickle.load(file)
+
+
+# flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/count/<string:session_id>/<string:category>', methods=['GET'])
+def get_count(session_id, category):
+    return jsonify(get_response_on_category_in_session(session_id,
+                                                       category,
+                                                       session))
+
+
+@app.route('/predict/<string:session_id>', methods=['GET'])
+def get_prediction(session_id):
+    return jsonify(get_prediction_response(session_id, session, production_model))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ gender_project/model.py
@@ -0,0 +1,37 @@
+# scientific libs
+import numpy as np
+import pandas as pd
+
+# file workers
+import pickle
+import os
+
+# transformers
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, QuantileTransformer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import FunctionTransformer
+# classifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+
+class CategoryTransformer(TransformerMixin):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def fit(self, X):
+        return self
+
+    def transform(self, X):
+        if (X is not None):
+            return [str(x) for x in X]
+        else:
+            return None
+
+    def fit_transform(self, X, y=None):
+        return self.fit(X).transform(X)
